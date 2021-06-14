850b8825064affbe8288d61768c806ea8ad384d2
Дмитрий <Torpedro1985@gmail.com>
2021-06-14 22:10:55 +0300

    message service initial commit

--- src/main/java/ru/reboot/dao/entity/MessageEntity.java
+++ src/main/java/ru/reboot/dao/entity/MessageEntity.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -28,4 +29,106 @@ public class MessageEntity {
 
     @Column(name = "last_access_time")
     private LocalDateTime lastAccessTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getMessageTimestamp() {
+        return messageTimestamp;
+    }
+
+    public void setMessageTimestamp(LocalDateTime messageTimestamp) {
+        this.messageTimestamp = messageTimestamp;
+    }
+
+    public LocalDateTime getLastAccessTime() {
+        return lastAccessTime;
+    }
+
+    public void setLastAccessTime(LocalDateTime lastAccessTime) {
+        this.lastAccessTime = lastAccessTime;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageEntity{" +
+                "id='" + id + '\'' +
+                ", sender='" + sender + '\'' +
+                ", recipient='" + recipient + '\'' +
+                ", content='" + content + '\'' +
+                ", messageTimestamp=" + messageTimestamp +
+                ", lastAccessTime=" + lastAccessTime +
+                '}';
+    }
+
+    public static class Builder {
+        private MessageEntity obj;
+
+        public Builder() {
+            obj = new MessageEntity();
+        }
+
+        public Builder setId(String id) {
+            obj.id = id;
+            return this;
+        }
+
+        public Builder setSender(String sender) {
+            obj.sender = sender;
+            return this;
+        }
+
+        public Builder setRecipient(String recipient) {
+            obj.recipient = recipient;
+            return this;
+        }
+
+        public Builder setContent(String content) {
+            obj.content = content;
+            return this;
+        }
+
+        public Builder setMessageTimestamp(LocalDateTime messageTimestamp) {
+            obj.messageTimestamp = messageTimestamp;
+            return this;
+        }
+
+        public Builder setLastAccessTime(LocalDateTime lastAccessTime) {
+            obj.lastAccessTime = lastAccessTime;
+            return this;
+        }
+
+        public MessageEntity build() {
+            return obj;
+        }
+    }
 }
